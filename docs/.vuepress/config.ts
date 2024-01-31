6eeb4a6775b64a5cd760f110bb386e592796d8d8
zhangyangeng <15035374951@126.com>
2024-01-31 19:03:16 +0800

    更新多语言

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,19 @@ import { NavItems4ZH } from './nav/index';
 
 export default defineConfig({
     title: 'Monaco Editor Docs',
+    dest: '../../dist',
+    locales: {
+        '/': {
+            lang: 'zh-CN',
+            title: 'Monaco Editor Docs',
+            description: 'Monaco Editor 官方文档翻译及整理'
+        },
+        '/en/': {
+            lang: 'en-US',
+            title: 'Monaco Editor Docs',
+            description: 'Monaco Editor official document translation and organization',
+        }
+    },
     themeConfig: {
         // i18n
         locales: {
@@ -11,12 +24,22 @@ export default defineConfig({
                 selectText: '选择语言',
                 ariaLabel: '选择语言',
                 nav: NavItems4ZH,
+                lastUpdated: '上次更新',
+                editLinkText: '在 GitHub 上编辑此页',
+            },
+            '/en/': {
+                label: 'English',
+                selectText: 'Languages',
+                ariaLabel: 'Select language',
+                nav: NavItems4ZH,
+                lastUpdated: 'Last updated',
+                editLinkText: 'Edit this page on GitHub',
             }
         },
-        // 最后更新时间
-        lastUpdated: 'Last Updated',
         // 仓库
         repo: 'zhangyangeng/MonacoEditorDocs',
-        smoothScroll: true
-    }
+        smoothScroll: true,
+    },
+    // 额外监听的文件变化
+    extraWatchFiles: ['.vuepress/nav/**', '.vuepress/sidebar/**'],
 });
